8d23a69cdb3c047a52d4c0bb554e7dc9d97933b9
khadziabdi1 <khadziabdi1@etf.unsa.ba>
2025-01-01 13:34:44 +0100

    Project init

---
+++ Source/GASPALS/Private/DeserEagle.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DeserEagle.h"
+
+// Sets default values
+ADeserEagle::ADeserEagle()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADeserEagle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADeserEagle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+}
+
